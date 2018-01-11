fcaa4cbd9bdc2b64325e1cff7a1407f430f8239e
jeinsoft <shingy.lee@gmail.com>
2018-01-11 16:43:15 +0900

    Edit

--- Book1/Unit8/number2/number2.go
+++ Book1/Unit8/number2/number2.go
@@ -13,7 +13,8 @@ func main() {
 
 	fmt.Println(a) // 9.000000000000004
 
-	const epsilon = 1e-14                   // Go 언어 머신 엡실론
-	fmt.Println(math.Abs(a-9.0) <= epsilon) // true: 연산한 값과 비교할 값의 차이를
+	const epsilon = 1e-14 // Go 언어 머신 엡실론
+	fmt.Println(math.Abs(a-9.0) <= epsilon)
+	// true: 연산한 값과 비교할 값의 차이를
 	// 구한 뒤 머신 엡실론보다 작거나 같은지 비교
 }
